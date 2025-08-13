f5006085dadd2dfccb5d049ff39250a4f49987e1
bft-codebot <support@boltfoundry.com>
2025-08-13 18:19:54 +0000

    Delete apps/boltFoundry directory completely

--- apps/boltFoundry/__generated__/__isograph/Query/EntrypointHome/param_type.ts
+++
@@ -1,8 +0,0 @@
-import { type Query__Home__output_type } from '../../Query/Home/output_type.ts';
-
-export type Query__EntrypointHome__param = {
-  readonly data: {
-    readonly Home: Query__Home__output_type,
-  },
-  readonly parameters: Record<PropertyKey, never>,
-};
--- apps/boltFoundry/__generated__/__isograph/Query/Formatter/output_type.ts
+++
@@ -1,4 +0,0 @@
-import type { ExtractSecondParam, CombineWithIntrinsicAttributes } from '@isograph/react';
-import type React from 'react';
-import { Formatter as resolver } from '../../../../components/Formatter.tsx';
-export type Query__Formatter__output_type = (React.FC<CombineWithIntrinsicAttributes<ExtractSecondParam<typeof resolver>>>);
