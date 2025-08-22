68cf98b5dce3e37268c99054113e5b6c7ed1b1bb
bft-codebot <support@boltfoundry.com>
2025-08-22 13:58:39 +0000

    feat: add BfSample list item component and types

---
+++ apps/boltfoundry-com/__generated__/__isograph/BfSample/SampleListItem/output_type.ts
@@ -0,0 +1,4 @@
+import type { ExtractSecondParam, CombineWithIntrinsicAttributes } from '@isograph/react';
+import type React from 'react';
+import { SampleListItem as resolver } from '../../../../isograph/components/BfSample/SampleListItem.tsx';
+export type BfSample__SampleListItem__output_type = (React.FC<CombineWithIntrinsicAttributes<ExtractSecondParam<typeof resolver>>>);
