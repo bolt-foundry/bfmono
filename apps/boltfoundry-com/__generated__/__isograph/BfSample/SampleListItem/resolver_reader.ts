51bcedab0c5afdae33ac78fad0b44148c397017d
bft-codebot <support@boltfoundry.com>
2025-08-22 13:58:39 +0000

    feat: add BfSample list item component and types

---
+++ apps/boltfoundry-com/__generated__/__isograph/BfSample/SampleListItem/resolver_reader.ts
@@ -0,0 +1,47 @@
+import type {ComponentReaderArtifact, ExtractSecondParam, ReaderAst } from '@isograph/react';
+import { BfSample__SampleListItem__param } from './param_type.ts';
+import { SampleListItem as resolver } from '../../../../isograph/components/BfSample/SampleListItem.tsx';
+
+const readerAst: ReaderAst<BfSample__SampleListItem__param> = [
+  {
+    kind: "Scalar",
+    fieldName: "id",
+    alias: null,
+    arguments: null,
+    isUpdatable: false,
+  },
+  {
+    kind: "Scalar",
+    fieldName: "name",
+    alias: null,
+    arguments: null,
+    isUpdatable: false,
+  },
+  {
+    kind: "Scalar",
+    fieldName: "telemetryData",
+    alias: null,
+    arguments: null,
+    isUpdatable: false,
+  },
+  {
+    kind: "Scalar",
+    fieldName: "collectionMethod",
+    alias: null,
+    arguments: null,
+    isUpdatable: false,
+  },
+];
+
+const artifact: ComponentReaderArtifact<
+  BfSample__SampleListItem__param,
+  ExtractSecondParam<typeof resolver>
+> = {
+  kind: "ComponentReaderArtifact",
+  fieldName: "BfSample.SampleListItem",
+  resolver,
+  readerAst,
+  hasUpdatable: false,
+};
+
+export default artifact;
