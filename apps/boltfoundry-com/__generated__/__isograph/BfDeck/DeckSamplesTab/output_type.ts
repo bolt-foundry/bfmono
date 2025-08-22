29192c0c5be3fc2e2ed8c46f76f3d12fc051ea85
bft-codebot <support@boltfoundry.com>
2025-08-22 13:58:39 +0000

    feat: add deck samples tab component types

---
+++ apps/boltfoundry-com/__generated__/__isograph/BfDeck/DeckSamplesTab/output_type.ts
@@ -0,0 +1,4 @@
+import type { ExtractSecondParam, CombineWithIntrinsicAttributes } from '@isograph/react';
+import type React from 'react';
+import { DeckSamplesTab as resolver } from '../../../../isograph/components/BfDeck/DeckSamplesTab.tsx';
+export type BfDeck__DeckSamplesTab__output_type = (React.FC<CombineWithIntrinsicAttributes<ExtractSecondParam<typeof resolver>>>);
---
+++ apps/boltfoundry-com/__generated__/__isograph/BfDeck/DeckSamplesTab/param_type.ts
@@ -0,0 +1,10 @@
+import { type BfDeck__DeckSamplesList__output_type } from '../../BfDeck/DeckSamplesList/output_type.ts';
+
+export type BfDeck__DeckSamplesTab__param = {
+  readonly data: {
+    readonly id: string,
+    readonly name: (string | null),
+    readonly DeckSamplesList: BfDeck__DeckSamplesList__output_type,
+  },
+  readonly parameters: Record<PropertyKey, never>,
+};
